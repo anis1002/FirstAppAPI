5d9c4c8991e7f1f1c47ae44ae236f1f2e692532d
anis1002 <anisskikda938@gmail.com>
2025-05-18 14:24:19 +0100

    init project

---
+++ src/main/java/com/api/FirstAppAPI/FirstAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.FirstAppAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/FirstAppAPI/FirstAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.FirstAppAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
